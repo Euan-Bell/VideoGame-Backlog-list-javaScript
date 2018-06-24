a61224f28b4cb72dca112d9fcbbf288c5a708c6a
Euan <bell_designs@outlook.com>
2018-06-24 15:36:40 +0100

    add and remove function working

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('javaScript loaded');
 
   const form = document.querySelector('#new-item-form');
 
   const handleFormSubmit = function(event){
     event.preventDefault();
     console.dir(event.target.platform.value);
-    const gameTitle = event.target.gameTitle.value);
-    const publisher = event.target.publisher.value);
-    const platform = event.target.platform.value);
+    const gameTitle = event.target.gameTitle.value;
+    const publisher = event.target.publisher.value;
+    const platform = event.target.platform.value;
 
     const videoGameBacklog = document.querySelector('#videoGame-backlog');
     const div = document.createElement('h3');
     videoGameBacklog.appendChild(div);
     div.textContent = `${gameTitle} ${publisher} ${platform}`;
-    this.reset();
+    // this.reset();
 
   }
+
+  form.addEventListener('submit', handleFormSubmit);
+
+  const removeAll = document.querySelector('#remove-all');
+
+  const handleRemoveAll = function(event){
+    const videoGameBacklog = document.querySelector('#videoGame-backlog');
+    videoGameBacklog.innerHTML = ''
+  }
+  removeAll.addEventListener('click', handleRemoveAll);
 })
